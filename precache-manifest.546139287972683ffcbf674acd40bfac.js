e516fcd4426c4d165ad2ffca982afed1848c682d
Your Name <nikolovskialexxandar@gmail.com>
2020-01-10 04:35:15 +0100

    Updates

---
+++ precache-manifest.546139287972683ffcbf674acd40bfac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1494fac14f633e32b105af227510f27",
+    "url": "/qa-app/index.html"
+  },
+  {
+    "revision": "981072c6607f1c6513db",
+    "url": "/qa-app/static/css/main.a0123859.chunk.css"
+  },
+  {
+    "revision": "ecd113068cf9a5d10ce6",
+    "url": "/qa-app/static/js/2.ab056cc3.chunk.js"
+  },
+  {
+    "revision": "981072c6607f1c6513db",
+    "url": "/qa-app/static/js/main.456c8371.chunk.js"
+  },
+  {
+    "revision": "e8a8418407530ba73a90",
+    "url": "/qa-app/static/js/runtime-main.96d2f731.js"
+  }
+]);
